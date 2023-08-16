a1855d56207c18e1bab91a5f96b4b4ddd68aded7
Vaishnavi-koganur <131569855+Vaishnavi-koganur@users.noreply.github.com>
2023-08-16 11:30:01 +0530

    Add files via upload
    Javascript part is embedded in html file

---
+++ photo/photography.php
@@ -0,0 +1,18 @@
+<?php
+$name1=$_POST["name1"];
+$Email1=$_POST["Email1"];
+$PhoneNumber1=$_POST["PhoneNumber1"];
+$sex1=$_POST["sex1"];
+$sex2=$_POST["sex1"];
+$conName1=$_POST["conName1"];
+$add1=$_POST["add1"];
+
+?>
+<?php
+echo $name1."<br>";
+echo $Email1."<br>";
+echo $PhoneNumber1."<br>";
+echo $sex1."<br>";
+echo $conName1."<br>";
+echo $add1."<br>";
+?>
